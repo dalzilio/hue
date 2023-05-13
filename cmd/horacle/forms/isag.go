1c015cd4c12da181ad638ef69593513d32972935
Silvano Dal Zilio <2209941+dalzilio@users.noreply.github.com>
2023-05-13 12:32:12 +0200

    changed horacle/forms to output isEF and size for reachability formulas

--- cmd/horacle/forms/isag.go
+++ cmd/horacle/forms/isag.go
@@ -68,46 +68,43 @@ func answer(compet string, root string, dirfile string) {
 		os.Exit(1)
 		return
 	}
+	isEF := checkIfEFandSimplify(queries)
 	for k, q := range queries {
-		fmt.Printf("%s-%s-%02d %d\n", dirfile, compet, k, formula.Size(q.Formula))
+		fmt.Printf("%s-%s-%02d %s %d\n", dirfile, compet, k, isEF[k], formula.Size(q.Formula))
 	}
 }
 
-func checkIfEFandSimplify(compet string, root string, dirfile string) {
-	formulafile := filepath.Join(filepath.Join(root, dirfile), compet+".xml")
-	xmlFile, err := os.Open(formulafile)
-	if err != nil {
-		log.Fatal("Error opening file:", err)
-		os.Exit(1)
-		return
-	}
-	defer xmlFile.Close()
-	decoder := formula.NewDecoder(xmlFile)
-	queries, err := decoder.Build()
-	if err != nil {
-		log.Fatal("Error decoding Formula file:", err)
-		os.Exit(1)
-		return
-	}
-	var triv string
+// checkIfEFandSimplify returns a slice of 16 strings, one for each query,
+// telling us wether it is a EF or AG formula. Therefore, knowing the verdict,
+// we can decide if it is a CEX or INV. For th etime being, we disable the
+// simplification because it can be very time consuming (I need to better
+// integrate a timeout using contexts inside the BDD library).
+func checkIfEFandSimplify(queries []formula.Query) []string {
+	// var triv string
+	res := make([]string, len(queries))
 	for k, q := range queries {
 		// added a 60s timeout, for e.g. DrinkVendingMachine-PT-10
-		q = doSimplify(q, compet)
 
-		if q.IsTrivial() {
-			rez := q.Formula.(formula.BooleanConstant)
-			if rez {
-				triv = "CSTTRUE"
-			} else {
-				triv = "CSTFALSE"
-			}
-		} else {
-			triv = "COMPLEX"
-		}
+		// REMOVED: I removed, for the time being, test for trivial formulas
+		// because it is very time consuming
+
+		// q = doSimplify(q, compet)
+
+		// if q.IsTrivial() {
+		// 	rez := q.Formula.(formula.BooleanConstant)
+		// 	if rez {
+		// 		triv = "CSTTRUE"
+		// 	} else {
+		// 		triv = "CSTFALSE"
+		// 	}
+		// } else {
+		// 	triv = "COMPLEX"
+		// }
 		if q.IsEF {
-			fmt.Printf("%s-%s-%02d EF %s\n", dirfile, compet, k, triv)
+			res[k] = "EF"
 		} else {
-			fmt.Printf("%s-%s-%02d AG %s\n", dirfile, compet, k, triv)
+			res[k] = "AG"
 		}
 	}
+	return res
 }
